45819167b79dc7164f83db1eef0cc3da7036e4b3
Aleksandra <aleksandra26@onet.eu>
2023-01-10 16:52:40 +0100

    Dodaj pliki projektów.

---
+++ PhoneBookApp/PhoneBook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookApp
+{
+    public class PhoneBook
+    {
+        public List<Contact> Contacts = new List<Contact>();
+
+        public void AddContact(Contact contact)
+        {
+            Contacts.Add(contact);
+        }
+
+        public void DisplayContactByNumber(string number)
+        {
+            var searchByNumber = Contacts.FirstOrDefault(c => c.Number == number);
+            Console.WriteLine(searchByNumber.Name);
+        }
+
+        public void DisplayContactByPhrase(string phrase)
+        {
+            var searchByPhrase = Contacts.Where(c => c.Name.Contains(phrase)).ToList();
+           foreach(var con in searchByPhrase)
+            {
+                Console.WriteLine(con.Name);
+                Console.WriteLine(con.Number);
+            }
+        }
+
+        public void DeleteContactByName(string name)
+        {
+            var searchContact = Contacts.FirstOrDefault(c => c.Name==name);
+            
+            Contacts.Remove(searchContact);
+            
+
+        }
+    }
+}
---
+++ PhoneBookApp/Program.cs
@@ -0,0 +1,93 @@
+﻿
+using PhoneBookApp;
+
+Console.WriteLine("Hello in phone book app");
+
+Console.WriteLine("Press:");
+Console.WriteLine("1 - if you have add new contact");
+Console.WriteLine("2 - if you have show all contacts");
+Console.WriteLine("3 - if you have show name by number");
+Console.WriteLine("4 - if you have show contact by phrase");
+Console.WriteLine("5 - if you have delete existing contact");
+Console.WriteLine("x - if you have escape");
+
+
+var phoneBook= new PhoneBook();
+
+
+
+while(true)
+{
+    Console.WriteLine("Select operation");
+    var selectOption = Console.ReadLine();
+    switch (selectOption)
+    {
+        case "1":
+            
+            
+            while (true)
+            {
+                Console.WriteLine("Insert name");
+                var insertName = Console.ReadLine();
+                var lenName = insertName.Length;
+                Console.WriteLine("Insert phone number");
+                var insertNumber = Console.ReadLine();
+                var len = insertNumber.Length;
+                if(len!=9 & lenName<3)
+                {
+                Console.WriteLine("Invalid number or name");
+                Console.WriteLine("Insert correct number and name");
+                }
+                else if(len == 9 & lenName >= 3)
+                {
+                var contact = new Contact(insertName, insertNumber);
+                phoneBook.AddContact(contact);
+                    break;
+                }
+                
+            }
+           
+           
+            break;
+           
+
+        case "2":
+            
+            foreach (var con in phoneBook.Contacts)
+            {
+                Console.WriteLine($"Contact: {con.Name } {con.Number}");
+                
+            }
+            break;
+        
+        case "3":
+            Console.WriteLine("Insert serching name by number");
+            var number = Console.ReadLine();
+            phoneBook.DisplayContactByNumber(number);            
+            break;
+        
+        case "4":
+            Console.WriteLine("Insert serching name by phrase");
+            var namebyphrase= Console.ReadLine();
+            phoneBook.DisplayContactByPhrase(namebyphrase);
+            break;
+
+        case "5":
+            Console.WriteLine("Insert name to delete contact");
+            var deleteContact = Console.ReadLine();
+            phoneBook.DeleteContactByName(deleteContact);
+            break;       
+
+        case "x":
+            return;
+
+        default:
+            Console.WriteLine("Invalid number. Insert correct number");
+            break;
+    }
+        
+}
+
+
+
+
